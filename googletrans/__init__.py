f451f25e5af750117639b6579cbe9fd1ff114f6a
Paper Mail (Alex) <papermail@airmail.cc>
2019-05-10 15:20:14 +0300

    change version

--- googletrans/__init__.py
+++ googletrans/__init__.py
@@ -1,6 +1,6 @@
 """Free Google Translate API for Python. Translates totally free of charge."""
 __all__ = 'Translator',
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 from googletrans.client import Translator, AsyncTranslator
